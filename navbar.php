0bf3144532818bae550e1d16e46a54c71a37a0fb
Stephanyan <rabena.stephane@gmail.com>
2018-03-22 12:47:54 +0100

    Fix unappearing navbar burger + add logo into the navbar

--- navbar.php
+++ navbar.php
@@ -1,7 +1,9 @@
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <a class="navbar-brand" href="#">
+          <img src="vendor/images/logo/complete-white.png" alt="Soundbase logo" width=150>
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
