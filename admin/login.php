0c9c105690f4291de571c4e15823670c5ace1427
Stephanyan <rabena.stephane@gmail.com>
2018-03-22 04:34:49 +0100

    Add tmp(?) logos and some styling on admin login page

--- admin/login.php
+++ admin/login.php
@@ -17,7 +17,7 @@
 
   <body class="text-center">
     <form class="form-signin">
-      <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <img class="mb-4" src="../vendor/images/logo/icon-black.png" alt="" width="100" height="89">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
@@ -28,7 +28,7 @@
           <input type="checkbox" value="remember-me"> Remember me
         </label>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <button class="btn btn-lg btn-secondary btn-block" type="submit">Sign in</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
   </body>
