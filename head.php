4036c6ea975cdfafbd00f9b5d887b41aa53da552
Stephanyan <rabena.stephane@gmail.com>
2018-03-22 05:04:40 +0100

    Add favicon the main website

--- head.php
+++ head.php
@@ -5,6 +5,9 @@
 
     <title>Soundbase</title>
 
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="vendor/images/favicon/favicon.ico" type="image/ico">
+
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
