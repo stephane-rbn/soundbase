5455e182aef4b89382cb09e595686613b9f5fd2e
Stephanyan <rabena.stephane@gmail.com>
2018-03-22 05:06:28 +0100

    Add favicon in admin site

--- admin/head.php
+++ admin/head.php
@@ -3,6 +3,9 @@
   <head>
     <title>Admin page</title>
 
+    <link rel="shortcut icon" href="../vendor/images/favicon/favicon.ico" type="image/ico">
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
