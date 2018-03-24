80320a4c6ece68f40ac16e17b5df4e3229707767
Stephanyan <rabena.stephane@gmail.com>
2018-03-24 13:04:04 +0100

    Create conf.inc.php containing error messages (not implemented)

---
+++ conf.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+require "dbConst.php";
+
+$listOfErrors = [
+  1  => "Le prénom doit faire entre 3 et 60 caractères",
+  2  => "Le nom doit faire entre 3 et 60 caractères",
+  3  => "Le nom d'artiste doit faire entre 2 et 60 caractères",
+  4  => "L'email n'est pas valide",
+  5  => "L'email existe déjà",
+  6  => "Le format de la date d'anniversaire n'est pas correct",
+  7  => "La date d'anniversaire n'existe pas",
+  8  => "Vous devez avoir entre 13 et 150 ans inclus",
+  9  => "Le mot de passe doit faire entre 8 et 40 caractères",
+  10 => "Le mot de passe de confirmation ne correspond pas"
+];
