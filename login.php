a4ef09f9978e6707bc5f02423bd4488b3d46183b
Stephanyan <rabena.stephane@gmail.com>
2018-03-25 03:56:12 +0200

    Implement basic login form with php (no style at all)

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+require_once "conf.inc.php";
+require_once "functions.php";
+
+$_POST["email"] = strtolower($_POST["email"]);
+
+if (count($_POST) === 2 && isset($_POST["email"]) && isset($_POST["pwd"])) {
+
+  // Connection to database
+  $connection = connectDB();
+
+  // Query that get the password that matching with the email given
+  $query = $connection->prepare("SELECT * FROM MEMBER WHERE email=:toto");
+
+  // Execute the query
+  $query->execute([
+    "toto" => $_POST["email"]
+  ]);
+
+  // Fetch data with the query
+  $data = $query->fetch();
+
+  if (password_verify($_POST["pwd"], $data["password"])) {
+    header("Location: home.php");
+  } else {
+    echo "NOK";
+  }
+}
+
+?>
+
+<section>
+  <form method="POST">
+    <input type="text" name="email" placeholder="Votre email">
+    <input type="password" name="pwd" placeholder="Votre mot de passe">
+    <input type="submit" value="Se connecter">
+  </form>
+</section>
