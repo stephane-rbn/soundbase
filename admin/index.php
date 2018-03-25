23fd28b9a269d9b44eedf55a47403b059cb0190d
Angristan <angristan@users.noreply.github.com>
2018-03-25 18:55:54 +0200

    Merge login page and explode new index

--- admin/index.php
+++ admin/index.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Soundbase admin</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/simple-sidebar.css" rel="stylesheet">
-
-</head>
+<?php
+  include "header.php";
+?>
 
 <body>
 
@@ -52,4 +36,6 @@
 
 </body>
 
-</html>
+<?php
+  include "footer.php";
+?>
--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,22 @@
-<?php
-  include "head.php";
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Admin page</title>
+
+    <link rel="shortcut icon" href="../vendor/images/favicon/favicon.ico" type="image/ico">
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
 
   <body class="text-center">
     <form class="form-signin">
@@ -20,3 +36,4 @@
     </form>
   </body>
 
+</html>
