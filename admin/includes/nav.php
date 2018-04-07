8406dc86e7e7a89a876c819d6b000a3477d235ea
Angristan <angristan@users.noreply.github.com>
2018-04-08 00:49:02 +0200

    Remove sidebar's search bar.
    Useless in our use case.
    
    Also 100th commit \o/

--- admin/includes/nav.php
+++ admin/includes/nav.php
@@ -40,16 +40,6 @@
   <div class="navbar-default sidebar" role="navigation">
     <div class="sidebar-nav navbar-collapse">
       <ul class="nav" id="side-menu">
-        <li class="sidebar-search">
-          <div class="input-group custom-search-form">
-            <input type="text" class="form-control" placeholder="Search...">
-            <span class="input-group-btn">
-              <button class="btn btn-default" type="button">
-                <i class="fa fa-search"></i>
-              </button>
-            </span>
-          </div>
-        </li>
         <li>
           <a href="users.php">
             <i class="fa fa-users fa-fw"></i> Users</a>
