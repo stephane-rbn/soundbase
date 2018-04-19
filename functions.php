ad78e37fa76ac76ec502c650eb92521038fc4d90
Stephanyan <rabena.stephane@gmail.com>
2018-04-19 17:17:12 +0200

    Add some comments explaining functions job

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 require_once "conf.inc.php";
 
+// Connection to database
 function connectDB(){
 
   $dsn = DBDRIVER . ":host=" . DBHOST . ";dbname=" . DBNAME;
@@ -15,6 +16,7 @@ function connectDB(){
   return $connection;
 }
 
+// Check if a user is connected
 function isConnected() {
 
   if (isset($_SESSION["auth"])) {
@@ -24,16 +26,19 @@ function isConnected() {
   return false;
 }
 
+// Generate a token
 function createToken() {
   // random_bytes() returns a cryptographic secure pseudo-random bytes (string)
   // bin2hex() returns a converted binary data in hexadecimal representation
   return bin2hex(random_bytes(32));
 }
 
+// Fill signup form fields when submission failed
 function fillSessionField($field) {
   return isset($_SESSION["postForm"]) ? $_SESSION["postForm"][$field] : "";
 }
 
+// Display error messages in signup form (below fields)
 function isErrorPresent($errorNumber) {
   if (isset($_SESSION["errorForm"])) {
     for ($i = 0; $i < count($_SESSION["errorForm"]); $i++) {
@@ -48,6 +53,7 @@ function isErrorPresent($errorNumber) {
   return false;
 }
 
+// Display login error message when submission failed
 function loginErrorMessage() {
   if (isset($_SESSION["message"])) {
     echo '<div class="alert alert-warning" role="alert">' . $_SESSION["message"] . '</div>';
