48837f1e8800ab00a7df189da711231c56368cec
Angristan <Angristan@users.noreply.github.com>
2018-04-22 15:03:25 +0200

    Disable number of entries choice (for now)

--- admin/users.php
+++ admin/users.php
@@ -30,17 +30,7 @@
             <div class="panel-body">
               <div id="dataTables-example_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer">
                 <div class="row">
-                  <div class="col-sm-6">
-                    <div class="dataTables_length" id="dataTables-example_length">
-                      <label>Show <select name="dataTables-example_length" aria-controls="dataTables-example" class="form-control input-sm">
-                        <option value="10">10</option>
-                        <option value="25">25</option>
-                        <option value="50">50</option>
-                        <option value="100">100</option>
-                      </select> entries</label>
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
+                  <div class="col-sm-12">
                     <div id="dataTables-example_filter" class="dataTables_filter">
                       <label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="dataTables-example"></label>
                     </div>
