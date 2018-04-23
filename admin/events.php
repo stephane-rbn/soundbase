b92fc0fbf2f3ce56dab6d4b7bc72bec39c5adc31
Angristan <Angristan@users.noreply.github.com>
2018-04-23 02:26:37 +0200

    Create events.php

---
+++ admin/events.php
@@ -0,0 +1,185 @@
+<?php
+
+  include "includes/head.php";
+
+?>
+
+<body>
+  <div id="wrapper">
+    <?php include "includes/nav.php"; ?>
+    <div id="page-wrapper">
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header">Events</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              Event list
+            </div>
+            <div class="panel-body">
+              <div id="dataTables-example_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <div id="dataTables-example_filter" class="dataTables_filter">
+                      <!-- Search form, processed by the same page -->
+                      <form method="GET" action="">
+                        <input type="search" name="search" class="form-control input-sm" placeholder="Search...">
+                        <button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-search fa-fw"></i></button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+                <br>
+                <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Description</th>
+                      <th>Capacity</th>
+                      <th>Event date</th>
+                      <th>Member</th>
+                      <th>Edit</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                      $connection = connectDB();
+                      $sql = $connection->prepare("SELECT COUNT(*) FROM EVENTS");
+                      $sql->execute();
+                      $result = $sql->fetchAll();
+
+                      $entries = $result['0']['0']; // Number of entries
+                      $perPage = 10; // Number of entries per page
+                      $nbPages = ceil($entries/$perPage); // Number of pages
+
+                      // Page shown defaults to 1, otherwise based on "?page="
+                      // Checking $_GET['page'] is a possible page number to provent SQL injections
+                      if (isset($_GET['page']) && $_GET['page'] > 0 && $_GET['page'] <= $nbPages) {
+                        $cPage = $_GET['page'];
+                      } else {
+                        $cPage = 1;
+                      }
+
+                      $connection = connectDB();
+
+                      if (isset($_GET['search'])) {
+                        $searchQuery = $_GET['search'];
+                        $searchQuery = htmlspecialchars($searchQuery);
+
+                        $sql = $connection->prepare("SELECT * FROM MEMBER WHERE (`username` LIKE '%".$searchQuery."%') OR (`name` LIKE '%".$searchQuery."%') OR (`email` LIKE '%".$searchQuery."%') LIMIT ". (($cPage - 1) * $perPage) .", $perPage");
+
+                      } else {
+                        $sql = $connection->prepare("SELECT * FROM EVENTS LIMIT ". (($cPage - 1) * $perPage) .", $perPage");
+                      }
+                      $sql->execute();
+                      $result = $sql->fetchAll(\PDO::FETCH_ASSOC);
+
+                      foreach ($result as $track) {
+                        echo '<tr class="odd gradeX">';
+                        echo '<td>' . $track['id'];
+                        echo '<td>' . $track['name'];
+                        echo '<td>' . $track['description'];
+                        echo '<td>' . $track['capacity'];
+                        echo '<td>' . $track['event_date'];
+                        echo '<td>' . $track['member'];
+                        echo '<td><a href="event_edit.php?id=' . $track['id'] . '">Edit</a>';
+                        echo "</tr>";
+                      }
+                    ?>
+                  </tbody>
+                </table>
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="dataTables_info" id="dataTables-example_info" role="status" aria-live="polite">
+                      <?php
+                        $connection = connectDB();
+                        $sql = $connection->prepare("SELECT COUNT(*) FROM TRACK");
+                        $sql->execute();
+                        $result = $sql->fetchAll();
+
+                        $entries = $result['0']['0']; // Number of entries
+                        $perPage = 10; // Number of entries per page
+                        $nbPages = ceil($entries/$perPage); // Number of pages
+
+                        // Page shown defaults to 1, otherwise based on "?page="
+                        // Checking $_GET['page'] is a possible page number to provent SQL injections
+                        if (isset($_GET['page']) && $_GET['page'] > 0 && $_GET['page'] <= $nbPages) {
+                          $cPage = $_GET['page'];
+                        } else {
+                          $cPage = 1;
+                        }
+
+                        if ($cPage == $nbPages) {
+                          // On the last page, the last entry of the page will be the last entry
+                          echo "Showing ".(($cPage - 1) * $perPage + 1)." to ".$entries." of ".$entries." events";
+                        } else {
+                          echo "Showing ".(($cPage - 1) * $perPage + 1)." to ".(($cPage) * $perPage)." of ".$entries." events";
+                        }
+                      ?>
+                    </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="dataTables_paginate paging_simple_numbers" id="dataTables-example_paginate">
+                      <ul class="pagination">
+                        <?php
+                          $connection = connectDB();
+                          $sql = $connection->prepare("SELECT COUNT(*) FROM EVENTS");
+                          $sql->execute();
+                          $result = $sql->fetchAll();
+
+                          $entries = $result['0']['0']; // Number of entries
+                          $perPage = 10; // Number of entries per page
+                          $nbPages = ceil($entries/$perPage); // Number of pages
+
+                          // Page shown defaults to 1, otherwise based on "?page="
+                          // Checking $_GET['page'] is a possible page number to provent SQL injections
+                          if (isset($_GET['page']) && $_GET['page'] > 0 && $_GET['page'] <= $nbPages) {
+                            $cPage = $_GET['page'];
+                          } else {
+                            $cPage = 1;
+                          }
+
+                          if ($cPage == 1) {
+                            // Disable previous button if first page
+                            echo '<li class="paginate_button previous disabled" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_previous"><a>Previous</a></li>';
+                          } else {
+                            echo '<li class="paginate_button previous" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_previous"><a href="?page='.($cPage - 1).'">Previous</a></li>';
+                          }
+
+                          for ($i = 1; $i <= $nbPages; $i++) {
+                            if ($i == $cPage) {
+                              // Set the current page number as active
+                              echo '<li class="paginate_button active" aria-controls="dataTables-example" tabindex="0"><a href="?page='.$i.'">'.$i.'</a></li>';
+                            } else {
+                              echo '<li class="paginate_button" aria-controls="dataTables-example" tabindex="0"><a href="?page='.$i.'">'.$i.'</a></li>';
+                            }
+                          }
+
+                          if ($cPage == $nbPages) {
+                            // Disable next button if last page
+                            echo '<li class="paginate_button next" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_next"><a>Next</a></li>';
+                          } else {
+                            echo '<li class="paginate_button next" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_next"><a href="?page='.($cPage + 1).'">Next</a></li>';
+                          }
+
+                        ?>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+<?php
+  include "includes/footer.php";
+?>
