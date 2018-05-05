7814fafb17294b988b6ab92e4402da5b3aa98b1e
Stephanyan <rabena.stephane@gmail.com>
2018-05-05 23:12:33 +0200

    Move edit-profile.file upload processing code into a new file
    - Create script/uploadProfilePhotos.php

--- edit-profile.php
+++ edit-profile.php
@@ -10,68 +10,7 @@
   require_once "conf.inc.php";
   include "head.php";
   include "navbar.php";
-
-  $connection = connectDB();
-
-  if (count($_POST) === 1
-    && isset($_POST['submit'])
-    && !empty($_FILES['avatar'])
-  ) {
-
-    $fileName    = $_FILES['avatar']['name'];
-    $fileTmpName = $_FILES['avatar']['tmp_name'];
-    $fileSize    = $_FILES['avatar']['size'];
-    $fileError   = $_FILES['avatar']['error'];
-
-    // Split the file name into an array by the separating the string into substrings using the '.' character
-    $fileNameArray = explode('.', $fileName);
-
-    // Get the last element of the array and make it in lower case
-    $fileExtension = strtolower(end($fileNameArray));
-
-    $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
-
-    // Check if the given file owns an allowed extension
-    if (in_array($fileExtension, $allowedExtensions)) {
-
-      // Check error at upload
-      if ($fileError === 0) {
-
-        // Check if the file size doesn't exceed 2MB
-        if ($fileSize < 2097152) {
-
-          $fileNewName = $_SESSION['id'] . "." . $fileExtension;
-          $fileDestination = "member/avatar/" . $fileNewName;
-
-          // Move the upload file from its tmp directory to its final destination
-          // $result's value is true when the move succeeds
-          $result = move_uploaded_file($fileTmpName, $fileDestination);
-
-          if ($result) {
-            $query = $connection->prepare('UPDATE MEMBER SET profile_photo_filename=:profile_photo_filename WHERE id=:id');
-
-            $query->execute([
-              'profile_photo_filename' => $_SESSION['id'] . "." . $fileExtension,
-              'id'                     => $_SESSION['id']
-            ]);
-
-            unset($_POST['submit']);
-
-            header('Location: profile.php');
-          }
-
-        } else {
-          die("Your file is too big!");
-        }
-      } else {
-        die("There was an error uploading your file!");
-      }
-    } else {
-      die("You can not upload files of this type!");
-    }
-  }
 ?>
-
 
     <div class="wrapper" id="wrapper-edit-profile">
       <h1>EDIT PROFILE</h1>
@@ -84,7 +23,7 @@
 
     <div class="container center_div register-form">
 
-      <form method="POST" enctype="multipart/form-data">
+      <form action="script/uploadProfilePhotos.php" method="POST" enctype="multipart/form-data">
         <!-- <div class="row">
           <div class="form-group col-sm-12">
             <label>PROFILE PICTURE</label>
