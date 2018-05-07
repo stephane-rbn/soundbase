0e70cf5c4a0b73f0132d65dcd654c53d1184521f
Stephanyan <rabena.stephane@gmail.com>
2018-05-07 15:09:02 +0200

    Create track uploads destination + update home.php

--- home.php
+++ home.php
@@ -12,26 +12,26 @@
   include "navbar.php";
 ?>
 
-<div class="wrapper" id="wrapper-login">
-  <h1>HOME</h1>
-  <h2>LISTEN TO YOUR FAVORITE ARTISTS</h2>
-</div>
-<div>
-  <br>
-  <?php
-    $trackData = sqlSelect("SELECT * FROM TRACK");
-    foreach ($trackData as $track) {
-      echo "<center>";
-      echo "<h2>".$track['title']."</h2>";
-      echo '<img src="uploads/album_cover/'.$track['photo_filename'].'" height="100px">';
-      echo '<audio controls>';
-      echo '<source src="uploads/tracks/'.$track['track_filename'].'" type="audio/flac">';
-      echo '</audio><br> Artist: '.$track['member'].'<br> Genre: '.$track['genre'].'<br> Publication: '.$track['publication_date'].'<br>';
-      echo '<hr>';
-      echo '</center>';
-    }
-  ?>
-</div>
+    <div class="wrapper" id="wrapper-login">
+      <h1>HOME</h1>
+      <h2>LISTEN TO YOUR FAVORITE ARTISTS</h2>
+    </div>
+    <div>
+      <br>
+      <?php
+        $trackData = sqlSelect("SELECT * FROM TRACK");
+        foreach ($trackData as $track) {
+          echo "<center>";
+          echo "<h2>" . $track['title'] . "</h2>";
+          echo '<img src="uploads/tracks/album_cover/'. $track['photo_filename'] . '" height="100px">';
+          echo '<audio controls>';
+          echo '<source src="uploads/tracks/files/' . $track['track_filename'] . '" type="audio/flac">';
+          echo '</audio><br> Artist: ' . $track['member'] . '<br> Genre: ' . $track['genre'] . '<br> Publication: ' . $track['publication_date'] . '<br>';
+          echo '<hr>';
+          echo '</center>';
+        }
+      ?>
+    </div>
 
 <?php
   include "footer.php";
