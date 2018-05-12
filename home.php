afb7ff8b49e32c16bf134d185ccc15795cf6a0b5
Angristan <Angristan@users.noreply.github.com>
2018-05-12 18:18:27 +0200

    Support for CSS Grid on home page

--- home.php
+++ home.php
@@ -17,20 +17,23 @@
       <h2>LISTEN TO YOUR FAVORITE ARTISTS</h2>
     </div>
     <div>
-      <br>
-      <?php
-        $trackData = sqlSelect("SELECT * FROM TRACK");
-        foreach ($trackData as $track) {
-          echo "<center>";
-          echo "<h2>" . $track['title'] . "</h2>";
-          echo '<img src="uploads/tracks/album_cover/'. $track['photo_filename'] . '" height="100px">';
-          echo '<audio controls>';
-          echo '<source src="uploads/tracks/files/' . $track['track_filename'] . '" type="audio/flac">';
-          echo '</audio><br> Artist: ' . $track['member'] . '<br> Genre: ' . $track['genre'] . '<br> Publication: ' . $track['publication_date'] . '<br>';
-          echo '<hr>';
-          echo '</center>';
-        }
-      ?>
+      <div class="home-feed">
+        <?php
+          $trackData = sqlSelect("SELECT * FROM TRACK");
+          foreach ($trackData as $track) {
+            echo '<div class="track-wrapper">';
+              echo "<h2>" . $track['member'] . " - " .$track['title'] . "</h2>";
+              echo '<img class="track-cover" src="uploads/tracks/album_cover/'. $track['photo_filename'] . '">';
+              echo '<div class="track-content">';
+                echo '<audio controls>';
+                echo '<source src="uploads/tracks/files/' . $track['track_filename'] . '" type="audio/flac">';
+                echo '</audio>';
+                echo '<h3 class="track-info"> '. $track['genre'] . ' - ' . $track['publication_date'] . '</span>';
+              echo '</div>';
+            echo '</div>';
+          }
+        ?>
+      </div>
     </div>
 
 <?php
