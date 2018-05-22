8fddee79f4b1401f990d47733d83c3e99f8003b1
Angristan <Angristan@users.noreply.github.com>
2018-05-22 14:09:11 +0200

    Caps

--- script/functions.js
+++ script/functions.js
@@ -2,7 +2,7 @@ function displayStrLength(maxLength) {
   const str = document.getElementById('textarea').value;
   const num = str.length;
   const count = document.getElementById('count')
-  count.innerHTML = 'number of characters entered :' + num;
+  count.innerHTML = 'Number of characters entered :' + num;
 
   if (num > maxLength) {
     document.getElementById('count').style.color = '#FF0000';
