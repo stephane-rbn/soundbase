a3a1fd48be44fa518e5e80c7aed321c338309fcb
Stephanyan <rabena.stephane@gmail.com>
2018-06-01 15:45:12 +0200

    Complete newPlaylist.php file + create savePlaylist.php

--- newPlaylist.php
+++ newPlaylist.php
@@ -14,23 +14,34 @@
 ?>
 
 
-    <div class="wrapper" id="wrapper-signup" style="background-image: url('vendor/images/newtrackpage/background.jpg');">
+    <div class="wrapper" id="wrapper-signup" style="background-image: url('vendor/images/newplaylistpage/background.jpg');">
       <h1>NEW TRACK</h1>
-      <h2>SHARE YOUR CREATION RIGHT NOW</h2>
+      <h2>FIND YOUR FAVORITE TRACKS EVERYWHERE</h2>
     </div>
 
     <div class="container center_div register-form">
-      <form method="POST" action="">
+      <form method="POST" action="script/savePlaylist.php">
 
         <div class="form-group">
           <label for="name">NAME</label>
-          <input type="text" class="form-control" placeholder="The Best Of 2018" name="name" value="<?php echo fillSessionField("name"); ?>" required="required"><?php
+          <input type="text" class="form-control" placeholder="ex: The Best Of 2017" name="name" value="<?php echo fillSessionField("name"); ?>" required="required"><?php
             if (isErrorPresent(19)) {
               echo '<p class="form_message_error">' . $listOfErrors[19] . '</p>';
             }?>
         </div>
 
+        <div class="form-group">
+          <button type="submit" class="btn btn-secondary">Submit</button>
+        </div>
+
       </form>
+
+      <?php
+        unset($_SESSION["postForm"]);
+        unset($_SESSION["errorForm"]);
+        unset($_SESSION["successUpdate"]);
+      ?>
+
     </div>
 
 <?php
