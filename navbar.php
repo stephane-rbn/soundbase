06389e23cf68cbbf18a3cbd8bb4bc6bcd72ad870
Angristan <angristan@pm.me>
2018-06-06 01:20:28 +0200

    Add contact.php link in navbar

--- navbar.php
+++ navbar.php
@@ -30,7 +30,7 @@
               <a class="nav-link" href="#">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+              <a class="nav-link" href="contact.php">Contact</a>
             </li>
             <?php if (isConnected()) { ?>
               <li class="nav-item dropdown">
