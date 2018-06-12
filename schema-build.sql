a3b9b8fc5a52d50ea911f684192650982b588f24
Angristan <Angristan@users.noreply.github.com>
2018-06-12 15:22:54 +0200

    Add "like" table

--- schema-build.sql
+++ schema-build.sql
@@ -38,6 +38,13 @@ CREATE TABLE listening (
     PRIMARY KEY (member, track)
 );
 
+CREATE TABLE like (
+    member  INTEGER NOT NULL REFERENCES member (id),
+    track   INTEGER NOT NULL REFERENCES track (id),
+    counter INTEGER default 0,
+    PRIMARY KEY (member, track)
+);
+
 CREATE TABLE playlist (
     id     INTEGER PRIMARY KEY AUTO_INCREMENT,
     name   VARCHAR(60),
