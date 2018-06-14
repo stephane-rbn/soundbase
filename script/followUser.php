3e9c831064eff572b2d704c0e2bdce7c45308c31
LAymeric <aymeric.larvet@gmail.com>
2018-06-14 16:08:50 +0200

    Add the ability to follow and unfollow a member
    - add dynamic button in profile.php
    - create followUser.php and unfollowUser.php scripts

---
+++ script/followUser.php
@@ -0,0 +1,20 @@
+<?php
+  session_start();
+  require "../functions.php";
+
+  $connection = connectDB();
+
+  if (!isConnected()) {
+      header("Location: login.php");
+    } else {
+
+      // Query that insert the new subscription
+      $query = $connection->prepare("INSERT INTO subscription (member_following,member_followed) VALUES (:member_following,:member_followed)");
+
+      $query->execute([
+        "member_following" => $_SESSION["id"],
+        "member_followed"  => $_GET["id"]
+      ]);
+
+      header("Location: ../profile.php?username=" . $_GET["username"]);
+  }
