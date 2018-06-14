19267b5b647c5f1288d74ae12311aa172d381829
Stephanyan <rabena.stephane@gmail.com>
2018-06-15 01:33:35 +0200

    Add error messages in user_edit.php form

--- admin/user_edit.php
+++ admin/user_edit.php
@@ -9,13 +9,22 @@
     <?php include "includes/nav.php"; ?>
     <div id="page-wrapper">
       <div class="row">
+        <br>
+        <?php
+          if (isset($_SESSION["message"])) {
+            fillAllFieldsErrorMessage();
+          } else {
+            successfulUpdateMessage();
+          }
+        ?>
         <div class="col-lg-12">
-          <h1 class="page-header"> Edit
+          <h1 class="page-header">Edit
             <?php
               $result = sqlSelect("SELECT username FROM member WHERE id='".$_GET['id']."'");
               echo $result['username'];
             ?>
           </h1>
+          <?php if (isset($_SESSION["errorForm"])) { var_dump($_SESSION["errorForm"]); } ?>
         </div>
       </div>
       <div class="row">
@@ -31,48 +40,113 @@
                     <div class="form-group">
                       <label>Username</label>
                       <input class="form-control" name="username" value="<?php
-                        $result = sqlSelect("SELECT username FROM member WHERE id='".$_GET['id']."' ");
-                        echo $result['username'];
-                      ?>" required="required">
+                        if (!isset($_SESSION["postForm"]["username"])) {
+                          $result = sqlSelect("SELECT username FROM member WHERE id='".$_GET['id']."' ");
+                          echo $result['username'];
+                        } else {
+                          echo $_SESSION["postForm"]["username"];
+                        }
+                        ?>" required="required">
+                        <?php
+                          if (isErrorPresent(2)) {
+                            echo '<p class="form_message_error">' . $listOfErrors[2] . '</p>';
+                          } else if (isErrorPresent(10)) {
+                            echo '<p class="form_message_error">' . $listOfErrors[10] . '</p>';
+                          } else {
+                            echo "";
+                          }
+                        ?>
                     </div>
                     <div class="form-group">
                       <label>Name</label>
                       <input class="form-control" name="name" value="<?php
-                        $result = sqlSelect("SELECT name FROM member WHERE id='".$_GET['id']."' ");
-                        echo $result['name'];
-                      ?>" required="required">
+                        if (!isset($_SESSION["postForm"]["name"])) {
+                          $result = sqlSelect("SELECT name FROM member WHERE id='".$_GET['id']."' ");
+                          echo $result['name'];
+                        } else {
+                          echo $_SESSION["postForm"]["name"];
+                        }
+                        ?>" required="required">
+                        <?php
+                          if (isErrorPresent(1)) {
+                            echo '<p class="form_message_error">' . $listOfErrors[1] . '</p>';
+                          }
+                        ?>
                     </div>
                     <div class="form-group">
                       <label>Email</label>
                       <input type="email" class="form-control" name="email" value="<?php
-                        $result = sqlSelect("SELECT email FROM member WHERE id='".$_GET['id']."' ");
-                        echo $result['email'];
-                      ?>" required="required">
+                        if (!isset($_SESSION["postForm"]["email"])) {
+                          $result = sqlSelect("SELECT email FROM member WHERE id='".$_GET['id']."' ");
+                          echo $result['email'];
+                        } else {
+                          echo $_SESSION["postForm"]["email"];
+                        }
+                        ?>" required="required">
+                      <?php
+                        if (isErrorPresent(6)) {
+                          echo '<p class="form_message_error">' . $listOfErrors[6] . '</p>';
+                        } else if (isErrorPresent(7)) {
+                          echo '<p class="form_message_error">' . $listOfErrors[7] . '</p>';
+                        } else {
+                          echo '';
+                        }
+                      ?>
                     </div>
                     <div class="form-group">
                       <label>Birthday</label>
                       <input type="date" class="form-control" name="birthday" value="<?php
-                        $result = sqlSelect("SELECT birthday FROM member WHERE id='".$_GET['id']."' ");
-                        echo $result['birthday'];
-                      ?>" required="required">
+                        if (!isset($_SESSION["postForm"]["birthday"])) {
+                          $result = sqlSelect("SELECT birthday FROM member WHERE id='".$_GET['id']."' ");
+                          echo $result['birthday'];
+                        } else {
+                          echo $_SESSION["postForm"]["birthday"];
+                        }
+                        ?>" required="required">
+                        <?php
+                          if (isErrorPresent(3)) {
+                            echo '<p class="form_message_error">' . $listOfErrors[3] . '</p>';
+                          } else if (isErrorPresent(4)) {
+                            echo '<p class="form_message_error">' . $listOfErrors[4] . '</p>';
+                          } else if (isErrorPresent(5)) {
+                            echo '<p class="form_message_error">' . $listOfErrors[5] . '</p>';
+                          } else {
+                            echo '';
+                          }
+                        ?>
                     </div>
                     <div class="form-group">
                       <label>Position</label>
                       <select class="form-control form-control-sm" name="position" required="required">
                       <?php
-                        $result = sqlSelect("SELECT position FROM member WHERE id='".$_GET['id']."' ");
-                        $status = $result['position'];
+                        if (!isset($_SESSION["postForm"]["position"])) {
+                          $result = sqlSelect("SELECT position FROM member WHERE id='".$_GET['id']."' ");
+                          $status = $result['position'];
 
-                        echo '<option value="0"' . (($status == 0) ? 'selected="selected"' : '') . '>User</option>';
-                        echo '<option value="1"' . (($status == 1) ? 'selected="selected"' : '') . '>Admin</option>';
-                        echo '<option value="2"' . (($status == 2) ? 'selected="selected"' : '') . '>Banned</option>';
+                          echo '<option value="0"' . (($status == 0) ? 'selected="selected"' : '') . '>User</option>';
+                          echo '<option value="1"' . (($status == 1) ? 'selected="selected"' : '') . '>Admin</option>';
+                          echo '<option value="2"' . (($status == 2) ? 'selected="selected"' : '') . '>Banned</option>';
+                        } else {
+                          echo '<option value="0"' . (($_SESSION["postForm"]["position"] == 0) ? 'selected="selected"' : '') . '>User</option>';
+                          echo '<option value="0"' . (($_SESSION["postForm"]["position"] == 1) ? 'selected="selected"' : '') . '>Admin</option>';
+                          echo '<option value="0"' . (($_SESSION["postForm"]["position"] == 2) ? 'selected="selected"' : '') . '>Banned</option>';
+                        }
                       ?>
                       </select>
-
-                      <input type="hidden" name="user_id" value="<?php echo $_GET['id'];?>">
+                      <?php
+                        if (isErrorPresent(24)) {
+                          echo '<p class="form_message_error">' . $listOfErrors[24] . '</p>';
+                        }
+                      ?>
                     </div>
+                    <input type="hidden" name="user_id" value="<?php echo $_GET['id'];?>">
                     <button type="submit" class="btn btn-default">Submit</button>
                   </form>
+                  <?php
+                    unset($_SESSION["postForm"]);
+                    unset($_SESSION["errorForm"]);
+                    unset($_SESSION["message"]);
+                  ?>
                 </div>
               </div>
             </div>
