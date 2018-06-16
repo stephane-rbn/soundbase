4bcbe81ae3b54b67ff8eae8eb1798e9fca56ac46
LAymeric <aymeric.larvet@gmail.com>
2018-06-16 16:03:04 +0200

    List all followed member tracks on home.php
    - create SQL query to get all tracks a followed members in home
    - add default message in home when a member doesn't follow anybody

--- home.php
+++ home.php
@@ -19,22 +19,32 @@
     <div>
       <div class="home-feed">
         <?php
-          $trackData = sqlSelectFetchAll("SELECT * FROM track");
-          foreach ($trackData as $track) {
-            $artistQuery = sqlSelect("SELECT name FROM member WHERE ID = ".$track['member']);
-            $artist = $artistQuery['name'];
-            echo '<div class="track-wrapper">';
-              echo "<a href='track.php?id=" . $track['id'] . "'>";
-                echo "<h2>" . $artist . " - " .$track['title'] . "</h2>";
-              echo "</a>";
-                echo '<img class="track-cover" src="uploads/tracks/album_cover/'. $track['photo_filename'] . '">';
-                echo '<div class="track-content">';
-                  echo '<audio controls>';
-                  echo '<source src="uploads/tracks/files/' . $track['track_filename'] . '" type="audio/flac">';
-                  echo '</audio>';
-                  echo '<h3 class="track-info"> '. $track['genre'] . ' - ' . $track['publication_date'] . '</span>';
-                echo '</div>';
-            echo '</div>';
+          $trackData = sqlSelectFetchAll('SELECT * FROM track WHERE member IN(SELECT member_followed FROM subscription WHERE member_following=' . $_SESSION['id'] . ")");
+
+          if (!empty($trackData)) {
+
+            foreach ($trackData as $track) {
+
+              $artistQuery = sqlSelect("SELECT name FROM member WHERE ID = ".$track['member']);
+
+              $artist = $artistQuery['name'];
+
+              echo '<div class="track-wrapper">';
+                echo "<a href='track.php?id=" . $track['id'] . "'>";
+                  echo "<h2>" . $artist . " - " .$track['title'] . "</h2>";
+                echo "</a>";
+                  echo '<img class="track-cover" src="uploads/tracks/album_cover/'. $track['photo_filename'] . '">';
+                  echo '<div class="track-content">';
+                    echo '<audio controls>';
+                    echo '<source src="uploads/tracks/files/' . $track['track_filename'] . '" type="audio/flac">';
+                    echo '</audio>';
+                    echo '<h3 class="track-info"> '. $track['genre'] . ' - ' . $track['publication_date'] . '</span>';
+                  echo '</div>';
+              echo '</div>';
+            }
+
+          } else {
+            echo "<p class='empty-home'>You can search for a member to follow his news</p>";
           }
         ?>
       </div>
