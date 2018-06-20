f2b062e03398a65a2c971e6cb5b99a20f837e9c6
Angristan <angristan@pm.me>
2018-06-20 20:10:41 +0200

    trackId > trackNumber
    This is not the track ID, but its number in the DOM

--- script/functions.js
+++ script/functions.js
@@ -56,22 +56,22 @@ function deleteTrack(trackId) {
   request.send('track=' + trackId);
 }
 
-function playlistPlay(trackId) {
+function playlistPlay(trackNumber) {
   // Get all <audio> tags on the page
   const tracks = document.getElementsByTagName('audio');
   // Get the current track
-  const track = tracks[trackId];
+  const track = tracks[trackNumber];
 
   // If the track is not the latest...
-  if (trackId < tracks.length - 1) {
-    const nextTrack = tracks[trackId + 1];
+  if (trackNumber < tracks.length - 1) {
+    const nextTrack = tracks[trackNumber + 1];
 
     track.onended = function() {
       // ...play the next track...
       nextTrack.play();
       // ...and call the function again for the next track!
       // (since it's triggrered the first time with onClick)
-      playlistPlay(trackId + 1);
+      playlistPlay(trackNumber + 1);
     };
   }
 }
