02f193e4f53cd730126f734de7538974bf84531a
LAymeric <aymeric.larvet@gmail.com>
2018-06-28 17:43:54 +0200

    add a token when a member registers to an event

--- script/registerInEvent.php
+++ script/registerInEvent.php
@@ -17,12 +17,18 @@
     // Database connection
     $connection = connectDB();
 
+    $ticketToken = createToken(64);
+
     // Query that inserts the registration
     $registrationQuery = $connection->prepare(
-      "INSERT INTO registration (member, events) VALUES (" . $_SESSION['id'] . ", " . $_POST["event_id"] .")"
+      "INSERT INTO registration (member, events, registration_token) VALUES (?, ?, ?)"
     );
 
-    $registrationQuery->execute();
+    $registrationQuery->execute([
+      $_SESSION['id'],
+      $_POST["event_id"],
+      $ticketToken
+    ]);
 
     $_SESSION["registredInEvent"] = true;
 
