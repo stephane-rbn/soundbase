7b0917b8dac49dc24444a40760b3342df0d21e34
Stephanyan <rabena.stephane@gmail.com>
2018-06-28 17:14:55 +0200

    Add the ability to remove a track from a playlist in ajax
    - create removeTrackFromPlaylist.php script containing the sql query
    - add removeTrackFromPlaylist() function in functions.js to remove the track with ajax
    - add remove button and modal in playlist.php

---
+++ script/removeTrackFromPlaylist.php
@@ -0,0 +1,36 @@
+<?php
+
+  session_start();
+  require "../functions.php";
+
+  xssProtection();
+
+  // Connection to database
+  $connection = connectDB();
+
+  $playlistQuery = $connection->query(
+    "SELECT id FROM playlist WHERE id={$_POST["playlist_id"]}"
+  );
+
+  $playlist = $playlistQuery->fetch(PDO::FETCH_ASSOC);
+
+  if (count($_POST) === 2
+    && $_POST["playlist_id"]
+    && $_POST["track_id"]
+  ) {
+
+    $removeInclusionQuery = $connection->query(
+      "DELETE FROM inclusion WHERE playlist={$_POST["playlist_id"]} AND track={$_POST["track_id"]}"
+    );
+
+    $res = $removeInclusionQuery->execute();
+
+    if ($res) {
+      http_response_code(200);
+    } else {
+      http_response_code(500);
+    }
+
+  } else {
+  http_response_code(400);
+}
