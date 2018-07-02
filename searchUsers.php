eb0cfad192e4ca8bd033e5a3ea947fc411d48e25
LAymeric <aymeric.larvet@gmail.com>
2018-07-02 12:47:31 +0200

    Add track on search bar

--- searchUsers.php
+++ searchUsers.php
@@ -20,36 +20,56 @@
       echo "<br>";
 
       $searchQuery = $_GET['username'];
-      $userSearch = sqlSelectFetchAll("SELECT * FROM member WHERE (`username` LIKE '%" . $searchQuery . "%') OR (`name` LIKE '%".$searchQuery."%') ");
+      $dataSearch = sqlSelectFetchAll("(SELECT member.id,member.name, member.username, member.profile_photo_filename, NULL as title, NULL as photo_filename FROM member WHERE (`username` LIKE '%" . $searchQuery . "%') OR (`name` LIKE '%".$searchQuery."%')) UNION (SELECT track.id, NULL as name, NULL as username, NULL as profile_photo_filename, track.title, track.photo_filename FROM track WHERE (`title` LIKE '%" . $searchQuery . "%'))");
 
-      foreach ($userSearch as $user) {
-        echo "<center>";
-          echo "<div class='row'>";
-            echo "<div class='col-md-2'>";
-              if ($user['profile_photo_filename']!="photo.png") {
-                echo "<center><a href='profile.php?username=" . $user["username"] . "'><img src='uploads/member/avatar/" . $user["profile_photo_filename"] . "' alt='profile picture' height=50 width=50></a></center>";
-              } else {
-                echo "<center><a href='profile.php?username=" . $user["username"] . "'><img src='http://placehold.it/200x200&text=Logo' alt='profile picture' height=50 width=50></a></center>";
-              }
+      foreach ($dataSearch as $data) {
+        if(isset($data['profile_photo_filename'])){
+          echo "<center>";
+            echo "<div class='row'>";
+              echo "<div class='col-md-2'>";
+                if ($data['profile_photo_filename']!="photo.png") {
+                  echo "<center><a href='profile.php?username=" . $data["username"] . "'><img src='uploads/member/avatar/" . $data["profile_photo_filename"] . "' alt='profile picture' height=50 width=50></a></center>";
+                } else {
+                  echo "<center><a href='profile.php?username=" . $data["username"] . "'><img src='http://placehold.it/200x200&text=Logo' alt='profile picture' height=50 width=50></a></center>";
+                }
+              echo "</div>";
+              echo "<div class='col-xs-2'>";
+                echo "<center><h5><b>name : </b></h5></center>";
+              echo "</div>";
+              echo "<div class='col-md-2'>";
+                echo "<center><h5>" . $data["name"] . "</h5></center>";
+              echo "</div>";
+              echo "<div class='col-md-2'>";
+                echo "<center><h5><b>username : </b></h5></center>";
+              echo "</div>";
+              echo "<div class='col-md-1'>";
+                echo "<center><h5>" . $data["username"] . "</h5></center>";
+              echo "</div>";
+              echo "<div class='col-md-2'>";
+                echo "<center><h5><a href='profile.php?username=" . $data["username"] . "'>Profile</a></h5></center>";
+              echo "</div>";
             echo "</div>";
-            echo "<div class='col-xs-2'>";
-              echo "<center><h5><b>name : </b></h5></center>";
+          echo "</center>";
+          echo "<hr>";
+        }else if(isset($data['photo_filename'])){
+          echo "<center>";
+            echo "<div class='row'>";
+              echo "<div class='col-md-2'>";
+                echo "<center><a href='track.php?id=" . $data["id"] . "'><img src='uploads/tracks/album_cover/" . $data["photo_filename"] . "' alt='profile picture' height=50 width=50></a></center>";
+              echo "</div>";
+                echo "<div class='col-xs-2'>";
+                  echo "<center><h5><b>title : </b></h5></center>";
+                echo "</div>";
+                echo "<div class='col-md-5'>";
+                  echo "<center><h5>" . $data["title"] . "</h5></center>";
+                echo "</div>";
+                echo "<div class='col-md-2'>";
+                  echo "<center><h5><a href='track.php?id=" . $data["id"] . "'>Track</a></h5></center>";
+                echo "</div>";
             echo "</div>";
-            echo "<div class='col-md-2'>";
-              echo "<center><h5>" . $user["name"] . "</h5></center>";
-            echo "</div>";
-            echo "<div class='col-md-2'>";
-              echo "<center><h5><b>username : </b></h5></center>";
-            echo "</div>";
-            echo "<div class='col-md-1'>";
-              echo "<center><h5>" . $user["username"] . "</h5></center>";
-            echo "</div>";
-            echo "<div class='col-md-2'>";
-              echo "<center><h5><a href='profile.php?username=" . $user["username"] . "'>Profile</a></h5></center>";
-            echo "</div>";
-          echo "</div>";
-        echo "</center>";
-        echo "<hr>";
+          echo "</center>";
+          echo "<hr>";
+        }
       }
     }
   } else {
