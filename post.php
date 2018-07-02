9d933a2f69657121ffbbd3d0f1cb32b4375e9690
Angristan <angristan@pm.me>
2018-07-02 21:29:53 +0200

    post new design

--- post.php
+++ post.php
@@ -23,6 +23,14 @@
       $query->execute();
 
       $post = $query->fetch(PDO::FETCH_ASSOC);
+
+      // Get the artist name
+      $authorQuery = $connection->prepare(
+        "SELECT name FROM member WHERE id = " . $post['member'] . ""
+      );
+      $authorQuery->execute();
+      $authorResult = $authorQuery->fetch(PDO::FETCH_ASSOC);
+      $author = $authorResult['name'];
     }
 
   }
@@ -36,23 +44,22 @@
     <div class="container">
 
       <div class="vertical-spacer"></div>
+        <div class="row justify-content-center">
 
-    <?php
-      echo "<center>";
-        echo "<div>";
-          echo "<h4>Post :<h4>";
-          echo "<h5>" . $post["content"] . "</h5>";
-          echo "<p>" . $post["publication_date"] . "</p>";
-        echo "</div>";
-      echo "</center>";
-    ?>
+          <?php
+            echo "<div class='col-lg-7 content-container'>";
+            echo "<h3>Post from $author</h3>";
+            echo "<p><i class='fas fa-calendar-alt'></i> {$post['publication_date']}</p>";
+            echo "<p class='alert alert-secondary'>{$post['content']}</p>";
+            echo "</div>";
+          ?>
 
-      <div class="vertical-spacer"></div>
+          </div>
 
       <div class="container center_div register-form">
 
         <div class="col-sm-12">
-        <div class="form-group">
+          <div class="form-group">
               <label for="content">Comment (280 characters max):</label>
               <textarea name="comment" rows="5" onkeyup="displayTextareaLength(280)" id="comment-content" class="form-control" placeholder ="Your comment..."></textarea>
               <p id="textarea-counter"></p>
