6cd4a2907b5e6b83b64afd29b73afd8f88255d46
Angristan <angristan@pm.me>
2018-07-02 17:17:24 +0200

    Rework track.php : new design, add missing features

--- track.php
+++ track.php
@@ -36,18 +36,66 @@
 
       <div class="vertical-spacer"></div>
 
-      <?php
-        echo "<center>";
-        // echo "<h2>" . $track['title'] . "</h2>";
-        echo "<h2>" . $track['title'] . "<a href='' style='color: #c8c8c8;' title='Add to a playlist' data-toggle='modal' data-target='#addToPlaylistModal'><i class='fas fa-plus fa-xs' style='margin-left: 10px;'></i></a></h2>";
-        echo '<img src="uploads/tracks/album_cover/'. $track['photo_filename'] . '" height="100px">';
-        echo '<audio controls>';
-        echo '<source src="uploads/tracks/files/' . $track['track_filename'] . '" type="audio/flac">';
-        echo '</audio><br> Artist: ' . $track['member'] . '<br> Genre: ' . $listOfGenres[$track['genre']] . '<br> Publication: ' . $track['publication_date'] . '<br>';
-        echo '</center>';
-        echo "<br>";
-      ?>
+          <div class="row justify-content-center">
 
+      <?php
+                // Track id for DOM
+                $trackNumber = 0;
+
+                // Get the number of listenings
+                $listeningsQuery = $connection->prepare(
+                  "SELECT COUNT(*) as listenings FROM listening WHERE track=" . $track['id']
+                );
+
+                // Get the number of likes
+                $likesQuery = $connection->prepare(
+                  "SELECT COUNT(*) as likes FROM likes WHERE track=" . $track['id']
+                );
+
+                // Check if the track is liked by the user
+                $isLikedQuery = $connection->prepare(
+                  "SELECT COUNT(*) as liked FROM likes WHERE track='" . $track['id'] . "' AND member='" . $_SESSION['id'] ."'"
+                );
+
+                // Get the artist name
+                $authorQuery = $connection->prepare(
+                  "SELECT name FROM member WHERE id = " . $track['member'] . ""
+                );
+
+                $likesQuery->execute();
+                $isLikedQuery->execute();
+                $listeningsQuery->execute();
+                $authorQuery->execute();
+
+                $likesResult = $likesQuery->fetch(PDO::FETCH_ASSOC);
+                $isLikedResult = $isLikedQuery->fetch(PDO::FETCH_ASSOC);
+                $listenings = $listeningsQuery->fetch(PDO::FETCH_ASSOC);
+                $authorResult = $authorQuery->fetch(PDO::FETCH_ASSOC);
+
+                $likes = $likesResult['likes'];
+                $isLiked = $isLikedResult['liked'];
+                $listeningsNumber = $listenings['listenings'];
+                $author = $authorResult['name'];
+
+                echo "<div class='col-lg-6 content-container'>";
+                  echo "<h3><a href='track.php?id={$track['id']}'>$author - {$track['title']}</a><a href='' style='color: #c8c8c8;' title='Add to a playlist' data-toggle='modal' data-target='#addToPlaylistModal-{$track['id']}'><i class='fas fa-plus fa-xs' style='margin-left: 10px;'></i></a></h3>";
+                  echo "<div><img class='content-image' src='uploads/tracks/album_cover/{$track['photo_filename']}'></div>";
+                  echo "<audio controls id='audio-track-$trackNumber' data-track-id='{$track['id']}'>";
+                    echo "<source src='uploads/tracks/files/{$track['track_filename']}' type='audio/mpeg'>";
+                  echo "</audio>";
+                  echo "<p><i class='fas fa-calendar-alt'></i> {$track['publication_date']}</p>";
+                  echo "<p>";
+                  echo "<span class='track-listenings'><i class='fas fa-play'></i>";
+                  echo "<span class='listening-number' id='listening-number-{$track['id']}'>$listeningsNumber</span>";
+                  echo "</span>";
+                  echo "<span class='track-likes' id='likes-{$track['id']}' onclick='likeTrack({$track['id']})'>";
+                  echo "<i class='" . (($isLiked == 1) ? 'fas' : 'far') . " fa-heart'></i>";
+                    echo "<span class='like-number' id='like-number-{$track['id']}'>$likes</span>";
+                  echo "</span>";
+                  echo "<p class='alert alert-secondary'>{$track['description']}</p>";
+                echo "</div>";
+        ?>
+      </div>
       <!-- Add to playlist button Modal -->
       <div class="modal fade" id="addToPlaylistModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -80,11 +128,6 @@
           </div>
         </div>
       </div>
-
-      <h4>Description</h4>
-      <p><?php echo $track["description"]; ?></p>
-
-      <div class="vertical-spacer"></div>
 
       <div class="container center_div register-form">
 
