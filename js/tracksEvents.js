2e1b91c154c819ea32fd62340e4c5f72529d7c57
Angristan <angristan@pm.me>
2018-07-02 20:30:32 +0200

    Add some semicolons

--- js/tracksEvents.js
+++ js/tracksEvents.js
@@ -9,20 +9,20 @@ if (tracks.length > 0) {
   for (trackNumber = 0; trackNumber < tracks.length; trackNumber++) {
     const track = tracks[trackNumber];
     const nextTrack = tracks[trackNumber + 1];
-    const trackId = document.getElementById('audio-track-' + trackNumber).getAttribute('data-track-id')
+    const trackId = document.getElementById('audio-track-' + trackNumber).getAttribute('data-track-id');
 
     // Add a listening
-    track.addEventListener('ended', function(){addListeningToTrack(trackId)})
+    track.addEventListener('ended', function(){addListeningToTrack(trackId)});
 
     // If the track is not the latest...
     if (trackNumber < tracks.length - 1) {
       // Play next track
-      track.addEventListener('ended', function(){nextTrack.play()})
+      track.addEventListener('ended', function(){nextTrack.play()});
     }
 
     // Pause all other tracks
     const currentTrackNumber = trackNumber;
     // This^ is needed otherwise trackNumber below is always = to tracks.length...
-    track.addEventListener('play', function(){pauseOtherTracks(tracks,currentTrackNumber)})
+    track.addEventListener('play', function(){pauseOtherTracks(tracks,currentTrackNumber)});
   }
 }
